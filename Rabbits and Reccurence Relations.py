4d3c7a90234d419c5708e326704a27822c3610cd
Smruthie Ramesh <70722786+smruthie@users.noreply.github.com>
2022-03-20 12:26:36 -0500

    Update Rabbits and Reccurence Relations.py

--- Rabbits and Reccurence Relations.py
+++ Rabbits and Reccurence Relations.py
@@ -8,9 +8,9 @@ while True:
 	Fn = onemonth_before + twomonths_before
 	rabbit_pairs.append(Fn)
 	n += 1
-	if n == 41:
+	if n == 49:
 		break
 
-print("The maximum you could enter is for 40 months!", "\n")	
+print("The maximum you could enter is for 48 months!", "\n") # wanted to give the user an upper-limit of 3 years
 months = int(input("Enter month for which you want to know how many rabbit pairs exist: "))
 print("\n", "There are", rabbit_pairs[months], "rabbit pairs in", months, "months!")
